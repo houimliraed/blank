2410a691c30be984b5120e73f50c18be4366e020
houimliraed@outlook.fr <houimliraed@outlook.fr>
2024-02-29 22:54:32 +0100

    variable diff

---
+++ jstesting/scope_lab.js
@@ -0,0 +1,12 @@
+// global scope
+var globalVar = "I am a global variable";
+let globallet = "I am a global let variable";
+const globalconst = "I am a global constant";
+
+{
+    //block scope
+    var blockVar = "I'm a block-scoped var";
+    let blockLet = "I'm a block-scoped let";
+    const blockConst = "I'm a block-scoped const";
+
+}
