4029b44e1c9079a8dde565b5b4d8f8fa579d5e79
houimliraed@outlook.fr <houimliraed@outlook.fr>
2024-03-12 01:09:10 +0000

    menu itteration added

--- arr/menu.js
+++ arr/menu.js
@@ -1,3 +1,19 @@
-const breakFastMenu = [];
-const mainCourseMenu = [];
-const dessertMenu = [];
+const breakFastMenu = ['Pancakes', 'Eggs Benedict', 'Oatmeal', 'Frittata'];
+const mainCourseMenu = ['Steak', 'Pasta', 'Burger', 'Salmon'];
+const dessertMenu = ['Cake', 'Ice Cream', 'Pudding', 'Fruit Salad'];
+
+const breakfastMenuItemsHTML = breakFastMenu.map((item, index) => `<p>Item ${index + 1}: ${item}</p>`).join('');
+document.getElementById('breakfastMenuItems').innerHTML = breakfastMenuItemsHTML;
+
+let mainCourseMenuItem = '';
+const mainCourseMenuHtml = mainCourseMenu.forEach((item , index)=>{
+    mainCourseMenuItem += `<p>${index + 1}:${item}</p>`;
+document.getElementById('maincourseMenuItems').innerHTML = mainCourseMenuItem;
+});
+
+let dessertMenuItem='';
+for(let i = 0;i<dessertMenu.length;i++)
+{
+    dessertMenuItem += `<p>${i+1} : ${dessertMenu[i]}</p>`
+}
+document.getElementById('dessertMenuItems').innerHTML = dessertMenuItem;
